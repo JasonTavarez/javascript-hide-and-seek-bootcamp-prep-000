ef35a42ddd7e340b20e0a8ac5880f03519262bda
Jason Santana <jsantana006@gmail.com>
2019-08-16 01:27:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector){
+  var s = document.querySelector(selector);
+  return s;
+}
 
+function nestedTarget(){
+  return document.querySelector('.target');
+}
+
+function deepestChild(){
+  
+  return document.querySelector('div#grand-node div div div div');
+}
+
+function increaseRankBy(n){
+  var a;
+  var list = document.getElementById('app').querySelectorAll('ul.ranked-list');
+  
+  for(var i = 0; i < list.length; i++){
+    a = parseInt(list[i].innerHTML = (i + 1));
+  }
+  return a;
+}
